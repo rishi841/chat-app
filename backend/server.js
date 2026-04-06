893a0e46cb9ce8fb978ec480678bee11482bc850
rishi841 <pandeyrishi7258@gmail.com>
2026-04-06 17:08:17 +0530

    removed .env and updated gitignore

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ const cors = require('cors')
 const {createServer}=require("http")
 const {Server}=require("socket.io")
 const chatSocket = require("./sockets/socket")
+
 const connectdb = require("./config/db")
 const roomRoutes=require("./routes/room.routes")
 
 const expServer= createServer(app)
 connectdb()
+PORT=process.env.PORT ||4500
 app.use(cors())
 app.use(express.json())
 app.use('/room',roomRoutes)
@@ -21,5 +23,5 @@ let io=new Server(expServer,{
 
 chatSocket(io)
 expServer.listen(3000,()=>{
-    console.log("server is running on port 3000")
+    console.log("server is running on port",PORT)
 })
